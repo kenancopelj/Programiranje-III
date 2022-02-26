54cd9e950dc97de2ee6e751e99f3c0b5cd926005
kenancopelj <kenancopelj@gmail.com>
2022-02-26 15:07:03 +0100

    rjesenje 2021-01-21

---
+++ 2020-01-21/Rjesenje/cSharpIntroWinForms/P8/KorisniciPolozeniPredmeti.cs
@@ -0,0 +1,143 @@
+﻿using cSharpIntroWinForms.P10;
+using cSharpIntroWinForms.P9;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cSharpIntroWinForms.P8
+{
+    public partial class KorisniciPolozeniPredmeti : Form
+    {
+        private Korisnik korisnik;
+
+        KonekcijaNaBazu konekcijaNaBazu = DLWMS.DB;
+
+        public KorisniciPolozeniPredmeti()
+        {
+            InitializeComponent();
+            dgvPolozeniPredmeti.AutoGenerateColumns = false;
+        }
+
+        public KorisniciPolozeniPredmeti(Korisnik korisnik) : this()
+        {
+            this.korisnik = korisnik;
+        }
+
+        private void KorisniciPolozeniPredmeti_Load(object sender, EventArgs e)
+        {
+            UcitajPredmete();
+            UcitajPolozene();
+        }
+
+        private void UcitajPredmete(List<Predmeti> lista = null)
+        {
+            cmbPredmeti.DataSource = null;
+            cmbPredmeti.DataSource = lista ?? konekcijaNaBazu.Predmeti.ToList();
+            cmbPredmeti.DisplayMember = "Naziv";
+            cmbPredmeti.ValueMember = "Id";
+        }
+
+        private void UcitajPolozene()
+        {
+            try
+            {
+                dgvPolozeniPredmeti.DataSource = null;
+                dgvPolozeniPredmeti.DataSource = korisnik.Uspjeh;
+            }
+            catch (Exception ex)
+            {
+                MboxHelper.PrikaziGresku(ex);
+            }
+        }
+
+        private void cbUcitajNepolozene_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbUcitajNepolozene.Checked)
+            {
+                var query = from p in konekcijaNaBazu.Predmeti.AsEnumerable()
+                            where !(from u in korisnik.Uspjeh
+                                    select u.Predmet.Id)
+                                    .Contains(p.Id)
+                            select p;
+                UcitajPredmete(query.ToList());
+            }
+            else
+                UcitajPredmete();
+        }
+
+        private void btnDodajPolozeni_Click(object sender, EventArgs e)
+        {
+            if(!VecPolozen())
+            {
+                var noviPolozeni = new KorisniciPredmeti()
+                {
+                    Korisnik = korisnik,
+                    Predmet = cmbPredmeti.SelectedItem as Predmeti,
+                    Ocjena = int.Parse(cmbOcjene.Text),
+                    Datum = dtpDatumPolaganja.Value.ToString("MM/dd/yyy")
+                };
+                konekcijaNaBazu.KorisniciPredmeti.Add(noviPolozeni);
+                konekcijaNaBazu.SaveChanges();
+                UcitajPolozene();
+            }
+            else
+            {
+                MessageBox.Show("Vec polozen", "Info");
+            }
+        }
+
+        private bool VecPolozen()
+        {
+            var odabraniPredmet = cmbPredmeti.SelectedItem as Predmeti;
+            foreach (var polozeni in korisnik.Uspjeh)
+                    if(odabraniPredmet.Id == polozeni.Predmet.Id)
+                        return true;
+            return false;
+        }
+
+        private void btnPrintajUvjerenje_Click(object sender, EventArgs e)
+        {
+            new frmIzvjestaj(korisnik).ShowDialog();
+        }
+
+        private async void btnASYNC_Click(object sender, EventArgs e)
+        {
+            var predmet = cmbPredmeti.SelectedItem as Predmeti;
+            Action action = () => UcitajPolozene();
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < 500; i++)
+                {
+                    konekcijaNaBazu.KorisniciPredmeti.Add(new KorisniciPredmeti()
+                    {
+                        Korisnik = korisnik,
+                        Predmet = predmet,
+                        Ocjena = 6,
+                        Datum = dtpDatumPolaganja.Value.ToString("dd/MM/yyyy")
+                    });
+                }
+                konekcijaNaBazu.SaveChanges();
+                MessageBox.Show("Uspjesno dodano 500 predmeta");
+                BeginInvoke(action);
+            });
+        }
+
+        private async void btnObrisi_Click(object sender, EventArgs e)
+        {
+            Action action = () => UcitajPolozene();
+            await Task.Run(() =>
+            {
+                konekcijaNaBazu.KorisniciPredmeti.RemoveRange(konekcijaNaBazu.KorisniciPredmeti);
+                konekcijaNaBazu.SaveChanges();
+                BeginInvoke(action);
+            });
+        }
+    }
+}
---
+++ 2020-01-21/Rjesenje/cSharpIntroWinForms/P8/frmIzvjestaj.cs
@@ -0,0 +1,83 @@
+﻿using cSharpIntroWinForms.P10;
+using cSharpIntroWinForms.P9;
+using Microsoft.Reporting.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cSharpIntroWinForms.P8
+{
+    public partial class frmIzvjestaj : Form
+    {
+        private Korisnik korisnik;
+        KonekcijaNaBazu baza = DLWMS.DB;
+
+        public frmIzvjestaj()
+        {
+            InitializeComponent();
+        }
+
+        public frmIzvjestaj(Korisnik korisnik):this()
+        {
+            this.korisnik = korisnik;
+        }
+
+        private void frmIzvjestaj_Load(object sender, EventArgs e)
+        {
+            var rpc = new ReportParameterCollection();
+            rpc.Add(new ReportParameter("Student", $"{korisnik.Ime} {korisnik.Prezime}"));
+
+            var tblPolozeni = new List<object>();
+
+            var nepolozeni = NepolozeniPredmeti();
+
+            for (int i = 0; i < korisnik.Uspjeh.Count; i++)
+            {
+
+                tblPolozeni.Add(new
+                {
+                    Rb = tblPolozeni.Count + 1,
+                    Predmet = korisnik.Uspjeh[i].Predmet.Naziv,
+                    Datum = korisnik.Uspjeh[i].Datum,
+                    Ocjena = korisnik.Uspjeh[i].Ocjena
+                });
+            }
+
+            for (int i = 0; i < nepolozeni.Count; i++)
+            {
+                tblPolozeni.Add(new
+                {
+                    Rb = tblPolozeni.Count + 1,
+                    Predmet = nepolozeni[i].Naziv,
+                    Datum = "NIJE POLOZENO",
+                    Ocjena = 0
+                });
+            }
+
+            var dataSource = new ReportDataSource();
+            dataSource.Value = tblPolozeni;
+            dataSource.Name = "dsPolozeni";
+
+            this.reportViewer1.LocalReport.DataSources.Add(dataSource);
+            this.reportViewer1.LocalReport.SetParameters(rpc);
+            this.reportViewer1.RefreshReport();
+        }
+
+        private List<Predmeti> NepolozeniPredmeti()
+        {
+            var query = from p in baza.Predmeti.AsEnumerable()
+                        where !(from u in korisnik.Uspjeh
+                                select u.Predmet.Id)
+                                .Contains(p.Id)
+                        select p;
+            return query.ToList();
+            
+        }
+    }
+}
