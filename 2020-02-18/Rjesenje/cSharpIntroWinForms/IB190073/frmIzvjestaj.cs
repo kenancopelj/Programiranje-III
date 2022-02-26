f204c6af3f5ffbf8eddb4ae060c101eb6e0da5af
kenancopelj <kenancopelj@gmail.com>
2022-02-26 15:20:02 +0100

    rjesenje 2020-02-18

---
+++ 2020-02-18/Rjesenje/cSharpIntroWinForms/IB190073/frmIzvjestaj.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using cSharpIntroWinForms.P8;
+using Microsoft.Reporting.WinForms;
+
+namespace cSharpIntroWinForms.IB190073
+{
+    public partial class frmIzvjestaj : Form
+    {
+        private dtoKorisnikIzvjestaj objekat;
+        public frmIzvjestaj()
+        {
+            InitializeComponent();
+        }
+
+        public frmIzvjestaj(dtoKorisnikIzvjestaj objekat) : this()
+        {
+            this.objekat = objekat;
+        }
+
+        private void frmIzvjestaj_Load(object sender, EventArgs e)
+        {
+            ReportParameterCollection rpc = new ReportParameterCollection();
+            rpc.Add(new ReportParameter("Ime", objekat.Ime));
+            rpc.Add(new ReportParameter("Prezime", objekat.Prezime));
+            rptViewer.LocalReport.SetParameters(rpc);
+
+
+            dsKorisnikIzvjestaja.tblPolozeniDataTable tbl = new dsKorisnikIzvjestaja.tblPolozeniDataTable();
+            foreach (var predmet in objekat.Polozeni)
+            {
+                dsKorisnikIzvjestaja.tblPolozeniRow noviRed = tbl.NewtblPolozeniRow();
+                noviRed.Rb = predmet.Id;
+                noviRed.Predmet = predmet.Predmet.Naziv;
+                noviRed.Ocjena = predmet.Ocjena;
+                noviRed.GodinaStudija = predmet.GodineStudija.ToString();
+                noviRed.DatumPolaganja = predmet.Datum;
+                tbl.Rows.Add(noviRed);
+            }
+
+            ReportDataSource izvorPodataka = new ReportDataSource();
+            izvorPodataka.Name = "dataSetPolozeniPredmeti";
+            izvorPodataka.Value = tbl;
+
+            rptViewer.LocalReport.DataSources.Add(izvorPodataka);
+            this.rptViewer.RefreshReport();
+        }
+    }
+}
---
+++ 2020-02-18/Rjesenje/cSharpIntroWinForms/IB200054/frmGodineStudijaIB200054.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace cSharpIntroWinForms.IB200054
+{
+    partial class frmGodineStudijaIB200054
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtNaziv = new System.Windows.Forms.TextBox();
+            this.cbAktivna = new System.Windows.Forms.CheckBox();
+            this.btnSpasi = new System.Windows.Forms.Button();
+            this.dgvGodineStudija = new System.Windows.Forms.DataGridView();
+            this.Naziv = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Aktivna = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.err = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvGodineStudija)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.err)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtNaziv
+            // 
+            this.txtNaziv.Location = new System.Drawing.Point(12, 34);
+            this.txtNaziv.Name = "txtNaziv";
+            this.txtNaziv.Size = new System.Drawing.Size(314, 22);
+            this.txtNaziv.TabIndex = 0;
+            // 
+            // cbAktivna
+            // 
+            this.cbAktivna.AutoSize = true;
+            this.cbAktivna.Location = new System.Drawing.Point(347, 36);
+            this.cbAktivna.Name = "cbAktivna";
+            this.cbAktivna.Size = new System.Drawing.Size(76, 21);
+            this.cbAktivna.TabIndex = 1;
+            this.cbAktivna.Text = "Aktivna";
+            this.cbAktivna.UseVisualStyleBackColor = true;
+            // 
+            // btnSpasi
+            // 
+            this.btnSpasi.Location = new System.Drawing.Point(464, 30);
+            this.btnSpasi.Name = "btnSpasi";
+            this.btnSpasi.Size = new System.Drawing.Size(87, 31);
+            this.btnSpasi.TabIndex = 2;
+            this.btnSpasi.Text = "Sacuvaj";
+            this.btnSpasi.UseVisualStyleBackColor = true;
+            this.btnSpasi.Click += new System.EventHandler(this.btnSpasi_Click);
+            // 
+            // dgvGodineStudija
+            // 
+            this.dgvGodineStudija.AllowUserToAddRows = false;
+            this.dgvGodineStudija.AllowUserToDeleteRows = false;
+            this.dgvGodineStudija.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvGodineStudija.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Naziv,
+            this.Aktivna});
+            this.dgvGodineStudija.Location = new System.Drawing.Point(8, 80);
+            this.dgvGodineStudija.Name = "dgvGodineStudija";
+            this.dgvGodineStudija.ReadOnly = true;
+            this.dgvGodineStudija.RowHeadersWidth = 51;
+            this.dgvGodineStudija.RowTemplate.Height = 24;
+            this.dgvGodineStudija.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvGodineStudija.Size = new System.Drawing.Size(543, 257);
+            this.dgvGodineStudija.TabIndex = 3;
+            this.dgvGodineStudija.CellContentDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvGodineStudija_CellContentDoubleClick);
+            // 
+            // Naziv
+            // 
+            this.Naziv.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Naziv.DataPropertyName = "Naziv";
+            this.Naziv.HeaderText = "Naziv";
+            this.Naziv.MinimumWidth = 6;
+            this.Naziv.Name = "Naziv";
+            this.Naziv.ReadOnly = true;
+            // 
+            // Aktivna
+            // 
+            this.Aktivna.DataPropertyName = "Aktivna";
+            this.Aktivna.HeaderText = "Aktivna";
+            this.Aktivna.MinimumWidth = 6;
+            this.Aktivna.Name = "Aktivna";
+            this.Aktivna.ReadOnly = true;
+            this.Aktivna.Width = 125;
+            // 
+            // err
+            // 
+            this.err.ContainerControl = this;
+            // 
+            // frmGodineStudijaIB200054
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(563, 349);
+            this.Controls.Add(this.dgvGodineStudija);
+            this.Controls.Add(this.btnSpasi);
+            this.Controls.Add(this.cbAktivna);
+            this.Controls.Add(this.txtNaziv);
+            this.Name = "frmGodineStudijaIB200054";
+            this.Text = "frmGodineStudijaIB200054";
+            this.Load += new System.EventHandler(this.frmGodineStudijaIB200054_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvGodineStudija)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.err)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNaziv;
+        private System.Windows.Forms.CheckBox cbAktivna;
+        private System.Windows.Forms.Button btnSpasi;
+        private System.Windows.Forms.DataGridView dgvGodineStudija;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Naziv;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Aktivna;
+        private System.Windows.Forms.ErrorProvider err;
+    }
+}
